fc87a631a41c75206df8bbfe4c66f29fd13575ab
omernaci <soydemir.naci@gmail.com>
2024-03-14 03:15:11 +0300

    report json file added.

---
+++ suite-api/src/main/java/com/ing/suiteapi/controller/GithubController.java
@@ -0,0 +1,24 @@
+package com.ing.suiteapi.controller;
+
+import com.ing.suiteapi.service.GithubClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/github/")
+public class GithubController {
+
+	private final GithubClient githubActionClient;
+
+	public GithubController(GithubClient githubActionClient) {
+		this.githubActionClient = githubActionClient;
+	}
+
+	@GetMapping("/reports")
+	public String getGithubArtifacts() throws IOException {
+		return githubActionClient.getCucumberReportAsJson();
+	}
+}
